0e5ad10eba4b1afec23d593d4df348b021b797ba
tatann007 <tatann05@gmail.com>
2019-04-20 17:52:55 -0400

    Instalación de react y lxml.

--- techk/apps/base/views.py
+++ techk/apps/base/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from bs4 import BeautifulSoup
+import requests
 
 def index(request):
+
+    source = requests.get('http://books.toscrape.com/index.html').text
+    soup = BeautifulSoup(source, 'lxml')
+
+    print(soup.prettify())
+
     return HttpResponse('Hello, world!')
